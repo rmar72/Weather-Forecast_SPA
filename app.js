236b166d6acf9fb37d1be4cd4ce04cdaf9cddd3d
rmar72 <rsolm72@gmail.com>
2017-09-04 23:42:19 -0600

    binding data

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ weatherApp.config(function ($routeProvider) {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
 });
 
 //SERVICES
@@ -29,6 +33,20 @@ weatherApp.controller('homeController', ['$scope','cityService', function($scope
     });
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope','$resource','cityService', '$routeParams', function($scope, $resource, cityService, $routeParams) {
   $scope.city = cityService.city;
+
+   $scope.days = $routeParams.days || 2;
+
+  $scope.weatherAPI = $resource(`http://api.openweathermap.org/data/2.5/find?&APPID=3fc425b141f9a9eafc794eacd234dbce`);
+  $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:$scope.days});
+  // console.log($scope.weatherResult);
+
+  $scope.convertToFahrenheit = function(degK){
+    return Math.round((1.8 * (degK-273)) + 32);
+  }
+
+  $scope.convertDate = function(dt){
+    return new Date(dt * 1000);
+  }
 }]);
