9e4feefa9d58ce2f5e6888d274e1e98107a19edd
rmar72 <rsolm72@gmail.com>
2017-09-04 21:31:30 -0600

    using custom service to bind values

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // ROUTES
 weatherApp.config(function ($routeProvider) {
-   
+
     $routeProvider
-    
     .when('/', {
         templateUrl: 'pages/home.htm',
         controller: 'homeController'
     })
-    
     .when('/forecast', {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
-    
+});
+
+//SERVICES
+weatherApp.service('cityService' ,function(){
+  this.city = "Denver, CO";
 });
 
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
-    
+weatherApp.controller('homeController', ['$scope','cityService', function($scope, cityService) {
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function(){
+      cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-    
+weatherApp.controller('forecastController', ['$scope','$resource','cityService', function($scope, $resource, cityService) {
+  $scope.city = cityService.city;
 }]);
