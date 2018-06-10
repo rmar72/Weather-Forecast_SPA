300ac31e707e4aa8bde608a3cc6d2ca9d3f12d54
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-06-09 18:58:57 -0600

    separating out weather api code into its own service for better scalability

--- controllers.js
+++ controllers.js
@@ -12,15 +12,12 @@ weatherApp.controller('homeController', ['$scope','$location','zipCodeService', 
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','zipCodeService', '$routeParams', function($scope, $resource, zipCodeService, $routeParams) {
+weatherApp.controller('forecastController', ['$scope','$resource','zipCodeService', 'weatherService', '$routeParams', function($scope, $resource, zipCodeService, weatherService, $routeParams) {
   $scope.zipcode = zipCodeService.zipcode;
 
   $scope.days = $routeParams.days || '2';
 
-  $scope.weatherAPI = $resource(`http://api.openweathermap.org/data/2.5/forecast`);
-  $scope.weatherResult = $scope.weatherAPI.get({ zip: $scope.zipcode, cnt: $scope.days, appid: 'f104c63011715cf24a9021add17cca97' });
-  // old query  $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:$scope.days});
-  // 06092018 working url = http://api.openweathermap.org/data/2.5/forecast?appid=f104c63011715cf24a9021add17cca97&cnt=5&zip=80640,us
+  $scope.weatherResults = weatherService.getWeather($scope.zipcode , $scope.days);
 
   $scope.convertToFahrenheit = function(degK){
     return Math.round((1.8 * (degK-273)) + 32);
--- services.js
+++ services.js
@@ -2,3 +2,13 @@
 weatherApp.service('zipCodeService' ,function(){
   this.zipcode = 80640;
 });
+
+weatherApp.weatherService('weatherService',['$resource', function($resource){
+  this.getWeather = function(zipcode, days){
+    var weatherAPI = $resource(`http://api.openweathermap.org/data/2.5/forecast`);
+    return weatherAPI.get({ zip: zipcode, cnt: days, appid: 'f104c63011715cf24a9021add17cca97' });
+    // old query  $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:$scope.days});
+    // 06092018 working url = http://api.openweathermap.org/data/2.5/forecast?appid=f104c63011715cf24a9021add17cca97&cnt=5&zip=80640,us
+  }
+
+}]);
